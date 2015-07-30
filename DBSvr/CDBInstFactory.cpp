5873192245f3d66b0e8894537700d4e448943de7
sguphil <sguphil@gmail.com>
2015-07-30 06:56:24 -0700

    add DBinstance factory make it reuse mysql connections

---
+++ DBSvr/CDBInstFactory.cpp
@@ -0,0 +1,2 @@
+#include "CDBInstFactory.hpp"
+
---
+++ DBSvr/CDBInstFactory.hpp
@@ -0,0 +1,32 @@
+#ifndef __CDBINSTFACTORY_H__
+#define __CDBINSTFACTORY_H__
+#include "../include/baseHeader.h"
+#include "../Factory/BaseFactory.h"
+#include "CSqlConn.hpp"
+
+class CDBInstFactory:public CBaseFactory<CSqlConn>
+{
+public:
+    void init(Int32 ItemNum, Int32 accNum, string ip, int32 port, string user, string passwd, string dbname)
+    {
+        m_nAccNum = accNum;
+        addItem(ItemNum, ip, port, user, passwd, dbname, true);
+    }
+private:
+    void addItem(Int32 num, string ip, int32 port, string user, string passwd, string dbname, bool flashTotalNum = false)
+    {
+        if (num > 0)
+        {
+            for (int i=0; i<num; i++)
+            {
+                CSqlConn *addPlayer = new CSqlConn(port, ip, user, passwd, dbname);
+                addPlayer->setFactory(this);
+                m_ItemList.push_back(addPlayer);
+                m_nItemLess++;
+                if (flashTotalNum)
+                    m_nTotalItem++;
+            }
+        }
+    }
+};
+#endif 

8adb346080486778163b3c5d66eb4d610fb81888
root <root@localhost.localdomain>
2015-08-01 21:38:22 -0700

    add send and recv bufqueue cpp file

---
+++ src/CRecvBuf.cpp
@@ -0,0 +1,38 @@
+#include "../include/CRecvBuf.hpp"
+
+CRecvBuf::CRecvBuf()
+{
+    
+}
+
+CRecvBuf::CRecvBuf(int32 size,int32 extraSize)
+{
+    init(size, extraSize);
+}
+
+CRecvBuf::~CRecvBuf()
+{
+
+}
+
+int32 CRecvBuf::getHead(PkgHeader *header)
+{
+    if (!checkHead(header))
+    {
+        return -1;
+    }
+    
+    memcpy(header, m_buffQueue.getReadPtr(), sizeof(*header));
+    return sizeof(*header);
+}
+
+int32 CRecvBuf::getMsg(char *buf, int32 bufsize)
+{
+    PkgHeader header;
+    if (!checkMsg(&header))
+    {
+        return -1;
+    }
+    m_buffQueue.GetMsg((char *)&header, sizeof(header));
+    return m_buffQueue.GetMsg(buf, bufsize);
+}
---
+++ src/CSendBuf.cpp
@@ -0,0 +1,22 @@
+#include "../include/CSendBuf.hpp"
+
+CSendBuf::CSendBuf()
+{
+}
+
+CSendBuf::~CSendBuf()
+{
+}
+
+bool CSendBuf::init(int32 size, int32 extraSize)
+{
+    return m_sendbuf.init(size, extraSize);
+}
+
+int32 CSendBuf::putMsg(PkgHeader *header, char* msg, int32 msgSize)
+{
+    char *buf = new char[msgSize + sizeof(*header)];
+    memcpy(buf, (char*)header, sizeof(*header));
+    memcpy(buf + sizeof(*header), msg, msgSize);
+    return m_sendbuf.pushMsg(buf,msgSize+sizeof(*header));
+}

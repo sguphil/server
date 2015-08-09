6ac50030ce4d3121fbe997e2c1fe9f741bf0511e
sguphil <sguphil@gmail.com>
2015-08-09 02:15:09 -0700

    add make.sh to manage Makefile

---
+++ make.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+arg=$1
+pwd=`pwd`
+if [ $arg == "all" ]; then 
+	echo "============start make AccSvr ==============="
+	cd AccSvr
+	make clean
+	make
+	echo "============start make testclient============"
+	cd ../testclient
+	make clean
+	make
+elif [ $arg = "clean" ]; then
+	cd $pwd
+	cd AccSvr
+	make clean
+	cd $pwd
+	cd testclient
+	make clean
+elif [ $arg = "accsvr" ]; then
+	cd $pwd 
+	cd AccSvr
+	make clean
+	make
+elif [ $arg = "tc" ]; then
+	cd $pwd
+	cd testclient
+	make clean
+	make
+else
+	echo "unknow args:"{$arg}
+fi
+
+
